cf4d1e9e1bbcfa4a1c5b100c1dfad1c6c298a1bd
Abimanyu Aria Putra <abimanyu_putra_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 15:00:49 +0700

    Add files via upload

---
+++ tes.java
@@ -0,0 +1,10 @@
+public class tes {
+
+    public static void main(String[] args) {
+        System.out.println("ini adalah file Tes");
+        int a = 1;
+        int b = 200;
+        int hasil = a+b;   
+        System.out.println("hasil dari " + a + "ditambah" + b + " adalah " + hasil);
+    }
+}
